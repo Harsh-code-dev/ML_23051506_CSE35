ed669a53151c7438be48f5dd28c61702aadc7493
Harsh-code-dev <hkumar070319@gmail.com>
2026-01-21 10:35:25 +0530

    Update gradient_discent.py

--- gradient_discent/gradient_discent.py
+++ gradient_discent/gradient_discent.py
@@ -1,23 +1,23 @@
 import numpy as np
 
-# Sample dataset
+
 X = np.array([1, 2, 3, 4, 5])
 y = np.array([3, 4, 2, 4, 5])
 
-# Reshape for consistency
+
 X = X.reshape(-1, 1)
 
-# Initialize parameters
+
 theta_0 = 0
 theta_1 = 0
 
-# Hyperparameters
-alpha = 0.01   # learning rate
-epochs = 1000   # iterations
+
+alpha = 0.01   
+epochs = 1000  
 
 m = len(X)
 
-# Gradient Descent
+
 for i in range(epochs):
     y_pred = theta_0 + theta_1 * X.flatten()
     
@@ -30,7 +30,7 @@ for i in range(epochs):
 print("Theta 0 (intercept):", theta_0)
 print("Theta 1 (slope):", theta_1)
 
-# Predict
+
 x_new = 6
 prediction = theta_0 + theta_1 * x_new
 print("Prediction for x = 6:", prediction)
